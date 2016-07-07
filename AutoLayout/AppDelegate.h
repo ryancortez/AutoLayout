7fee901e1d05526b8c7c78fc738923cb57f676df
ryancortez <ryancortez@mac.com>
2016-07-07 12:35:45 -0500

    Initial Commit

---
+++ AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Ryan Cortez on 7/7/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
